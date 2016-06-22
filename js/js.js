feab6a20870f1ebd2b300dc01c99e1ed90fd04c4
hermione <hermione@hermiones-MacBook-Pro.local>
2016-06-22 15:29:47 +0800

    first commit

---
+++ js/js.js
@@ -0,0 +1,19 @@
+var main = document.querySelector("main");
+var menu_icon = document.getElementById("menu_icon");
+var drawer = document.getElementById("drawer");
+
+
+menu_icon.addEventListener('click',
+function(e){
+	//alert("hover");
+	drawer.classList.toggle('open');
+	main.classList.toggle('down');
+	e.stopPropagation();
+})
+
+main.addEventListener('click',
+function(e){
+	//alert("hover");
+	drawer.classList.remove('open');
+	main.classList.remove('down');
+})
