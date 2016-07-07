7bc4f073e0460a1fa175df6c6cf4b7aa1c35ab25
hermione <hermione@hermiones-MacBook-Pro.local>
2016-07-07 12:37:59 +0800

    ddd

--- js/js.js
+++ js/js.js
@@ -1,29 +1,35 @@
 var main = document.querySelector("main");
 var menu_icon = document.getElementById("menu_icon");
 var content = document.getElementById("content");
+var header = document.querySelector("header");
+var drawer = document.querySelector("nav");
 
 
 menu_icon.addEventListener('click',
 function(e){
 		
 	//alert("pressed hamburger");
-	drawer.classList.toggle('left');
+drawer.classList.toggle('left');
 	
 menu_icon.classList.toggle('open');
-main.classList.toggle('left');
+content.classList.toggle('left');
+header.classList.toggle('left');
 	e.stopPropagation();
 })
 
 content.addEventListener('click',
 function(e){
 	//alert("press cotent!");
-	drawer.classList.remove('left');
-	main.classList.remove('left');
+	//drawer.classList.remove('open');
+	//main.classList.remove('left');
 	menu_icon.classList.remove('open');
+	content.classList.remove('left');
+	header.classList.remove('left');
+	drawer.classList.remove('left');
 	e.stopPropagation();
 })
 
 var header_h = $( "header" ).height();
-$("#banner").css('margin-top', $( "header" ).height() + "px");
+//$("#banner").css('margin-top', $( "header" ).height() + "px");
 
 
