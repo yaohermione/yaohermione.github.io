906d0eca3f12f0fad695c9f22dacfbd7b12f9be0
hermione <hermione@hermiones-MacBook-Pro.local>
2016-07-08 17:52:52 +0800

    dd

--- js/js.js
+++ js/js.js
@@ -32,4 +32,60 @@ function(e){
 var header_h = $( "header" ).height();
 //$("#banner").css('margin-top', $( "header" ).height() + "px");
 
+$("#test").click(function() {
+  $("header").addClass('header-up');
+});
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var header_height = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > header_height){
+        // Scroll Down
+        $("header").addClass('header-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $("header").removeclass('header-up');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
