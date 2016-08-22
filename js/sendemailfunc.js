e1160f96c43ec26e2faa7fe0138e5b810acb06ae
hermione <hermione@hermiones-MacBook-Pro.local>
2016-08-22 18:28:33 +0800

    ddd

--- js/sendemailfunc.js
+++ js/sendemailfunc.js
@@ -2,13 +2,14 @@
 	var senderEmail;
 	var subject;
 	var message;
+	var email_result_container;
 	function gotosendemail(){
 		
 		var sender = $("#sender").val();
 		var senderEmail = $("#senderEmail").val();
 		var subject = $("#subject").val();
 		var message = $("#message").val();
-
+		var email_result_container = document.getElementById("email-result-container");
 	 //alert("sendemail.php?sender="+sender+"&senderEmail="+senderEmail+"&subject="+subject+"&message="+message);
 		if(senderEmail&&message){
 			alert("both not null");
@@ -21,19 +22,16 @@
 				  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 				  }
 				xmlhttp.onreadystatechange=function(){
-				  //if (xmlhttp.readyState==4 && xmlhttp.status==200)
-				  	if(0)
+				  if (xmlhttp.readyState==4 && xmlhttp.status==200)
 					{
-						//alert("finished");
+						alert("finished");
 						//$("#email-result").html(xmlhttp.responseText);
-						var email_result_container = document.getElementById("email-result-container");
 						email_result_container.classList.remove('hide-result');
-						$("#email-result").html("Sent successfully!");
+						$("#email-result").html("<img src=\"img/tick.svg\">Sent successfully!");
 						senderEmail.classList.remove('wong-input');
 						message.classList.remove('wong-input');
 					}
-					else if(1){
-						var email_result_container = document.getElementById("email-result-container");
+					else{
 						email_result_container.classList.remove('hide-result');
 						$("#email-result").html("<div class=\"uil-ring-css\"><div></div></div>");
 					}
