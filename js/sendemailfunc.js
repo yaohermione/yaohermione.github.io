6f4063964dea8f8b6fdc4f62b6c971cd8a1af049
hermione <hermione@hermiones-MacBook-Pro.local>
2016-08-22 23:11:10 +0800

    s

--- js/sendemailfunc.js
+++ js/sendemailfunc.js
@@ -49,14 +49,14 @@
 			else if(senderEmail==""){
 				email_result_container.classList.add('hide-result');
 				alert("senderEmail="+senderEmail);
-					 senderEmail.classList.add('wong-input');
-					//$("#senderEmail").addClass('wong-input');
+				//$("#senderEmail").addClass('wong-input');
+document.getElementById("senderEmail").addClass('wong-input');
 			}
 			else if(message==""){
 				email_result_container.classList.add('hide-result');
 				alert("message="+message);
-					message.classList.add('wong-input');
-					//$("#message").addClass('wong-input');
+				//$("#message").addClass('wong-input');
+document.getElementById("message").addClass('wong-input');
 			}
 			
 
