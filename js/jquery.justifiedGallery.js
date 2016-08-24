150837b97a53ddf60d17afc1bc14aecbc83ea83d
hermione <hermione@hermiones-MacBook-Pro.local>
2016-08-24 17:35:12 +0800

    dd

--- js/jquery.justifiedGallery.js
+++ js/jquery.justifiedGallery.js
@@ -1085,8 +1085,8 @@
     thumbnailPath: undefined, /* If defined, sizeRangeSuffixes is not used, and this function is used to determine the
     path relative to a specific thumbnail size. The function should accept respectively three arguments: 
     current path, width and height */
-    rowHeight: 300,
-    maxRowHeight: 250, // negative value = no limits, number to express the value in pixels,
+    rowHeight: 70,
+    maxRowHeight: "200%", // negative value = no limits, number to express the value in pixels,
                           // '[0-9]+%' to express in percentage (e.g. 300% means that the row height
                           // can't exceed 3 * rowHeight)
     margins: 10,
