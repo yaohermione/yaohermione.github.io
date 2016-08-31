08f5eeb43209c886384a714d67568841c5c54cb5
hermione <hermione@hermiones-MacBook-Pro.local>
2016-08-31 14:04:29 +0800

    d

--- js/jquery.swipebox.js
+++ js/jquery.swipebox.js
@@ -822,16 +822,16 @@
 					// } );
 					//slide.addClass( 'uil-default-anim' );
 					$( '#swipebox-slider .slide' ).html("<div class=\"uil-default-css\" >\
-						<div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(0deg) translate(0,-60px);transform:rotate(0deg) translate(0,-60px);border-radius:10px;position:absolute;'></div>\
-    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(36deg) translate(0,-60px);transform:rotate(36deg) translate(0,-60px);border-radius:10px;position:absolute;'></div>\
-    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(72deg) translate(0,-60px);transform:rotate(72deg) translate(0,-60px);border-radius:10px;position:absolute;'></div>\
-    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(108deg) translate(0,-60px);transform:rotate(108deg) translate(0,-60px);border-radius:10px;position:absolute;'></div>\
-    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(144deg) translate(0,-60px);transform:rotate(144deg) translate(0,-60px);border-radius:10px;position:absolute;'></div>\
-    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(180deg) translate(0,-60px);transform:rotate(180deg) translate(0,-60px);border-radius:10px;position:absolute;'></div>\
-    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(216deg) translate(0,-60px);transform:rotate(216deg) translate(0,-60px);border-radius:10px;position:absolute;'></div>\
-    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(252deg) translate(0,-60px);transform:rotate(252deg) translate(0,-60px);border-radius:10px;position:absolute;'></div>\
-    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(288deg) translate(0,-60px);transform:rotate(288deg) translate(0,-60px);border-radius:10px;position:absolute;'></div>\
-    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(324deg) translate(0,-60px);transform:rotate(324deg) translate(0,-60px);border-radius:10px;position:absolute;'></div>\
+						<div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(0deg) translate(0,-60px);transform:rotate(0deg) translate(0,-0px);border-radius:10px;position:absolute;'></div>\
+    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(36deg) translate(0,-60px);transform:rotate(36deg) translate(0,-0px);border-radius:10px;position:absolute;'></div>\
+    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(72deg) translate(0,-60px);transform:rotate(72deg) translate(0,-0px);border-radius:10px;position:absolute;'></div>\
+    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(108deg) translate(0,-60px);transform:rotate(108deg) translate(0,-0px);border-radius:10px;position:absolute;'></div>\
+    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(144deg) translate(0,-60px);transform:rotate(144deg) translate(0,-0px);border-radius:10px;position:absolute;'></div>\
+    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(180deg) translate(0,-60px);transform:rotate(180deg) translate(0,-0px);border-radius:10px;position:absolute;'></div>\
+    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(216deg) translate(0,-60px);transform:rotate(216deg) translate(0,-0px);border-radius:10px;position:absolute;'></div>\
+    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(252deg) translate(0,-60px);transform:rotate(252deg) translate(0,-0px);border-radius:10px;position:absolute;'></div>\
+    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(288deg) translate(0,-60px);transform:rotate(288deg) translate(0,-0px);border-radius:10px;position:absolute;'></div>\
+    <div style='top:0px;left:0px;width:14px;height:40px;background:#ececec;-webkit-transform:rotate(324deg) translate(0,-60px);transform:rotate(324deg) translate(0,-0px);border-radius:10px;position:absolute;'></div>\
 						</div>");
 
 				} else {
