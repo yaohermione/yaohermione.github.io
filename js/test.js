228cc5e786be938a108ca112398a0982029d038c
hermione <hermione@hermiones-MacBook-Pro.local>
2016-10-20 18:09:51 +0800

    ddd

--- js/test.js
+++ js/test.js
@@ -98,7 +98,7 @@ function hasScrolled() {
     } else if(st < lastScrollTop && st > banner_height) {
             $("header").addClass('fixed').removeClass('header-up');
           $(".horizontal").addClass("dark-banner-text");
-          $(".horizontal").addClass("newcolor");
+          $(".horizontal").removeClass("newcolor");
         
     }else if(st < banner_height) {
         //console.log("this is st: "+st);
